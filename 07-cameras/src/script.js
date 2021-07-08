4d575fdbc1a784fdc7850e748e017dd636a8ecf9
Stefano Andriolo <stefano.andriolo@hotminds.it>
2021-07-08 08:57:44 +0200

    Finish chapter 7 exercises

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 // Scene
 const scene = new THREE.Scene()
@@ -22,9 +23,10 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+const canvasElement = document.querySelector('canvas.webgl')
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvasElement
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
@@ -34,12 +36,20 @@ const cursor = {
     y: 0
 }
 
+const controls = new OrbitControls(camera, canvasElement)
+controls.enableDamping = true
+controls.rotateSpeed = 2
+controls.zoomSpeed = 1.5
+controls.panSpeed = 1.5
+controls.dampingFactor = 0.1
+
 const tick = () => {
     // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
     // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
     // camera.position.y = cursor.y * 3
 
     camera.lookAt(mesh.position)
+    controls.update()
 
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
@@ -47,7 +57,7 @@ const tick = () => {
 
 tick()
 
-window.addEventListener('pointermove', ev => {
-    cursor.x = ev.clientX / sizes.width - 0.5
-    cursor.y = -(ev.clientY / sizes.height - 0.5)
-});
+// canvasElement.addEventListener('pointermove', ev => {
+//     cursor.x = ev.clientX / sizes.width - 0.5
+//     cursor.y = -(ev.clientY / sizes.height - 0.5)
+// });
