92870b743ee3c6b52ab3144f6dff81624baf8819
Stefano Andriolo <stefano.andriolo@hotminds.it>
2021-07-13 23:23:03 +0200

    Add exercise 19

---
+++ 19-raycaster/src/script.js
@@ -0,0 +1,177 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = -2
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object3.position.x = 2
+
+scene.add(object1, object2, object3)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Mouse handlers
+ */
+const mouse = new THREE.Vector2()
+window.addEventListener('mousemove', ev => {
+  mouse.x = ev.clientX / sizes.width * 2 - 1
+  mouse.y = - (ev.clientY / sizes.height) * 2 + 1
+})
+
+let currentIntersect = null
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+  
+  // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+  // const rayDirection = new THREE.Vector3(10, 0, 0)
+  // // the ray direction has to be normalized
+  // rayDirection.normalize()
+  // raycaster.set(rayOrigin, rayDirection)
+
+  raycaster.setFromCamera(mouse, camera)
+
+  const objectsToTest = [object1, object2, object3]
+  const intersects = raycaster.intersectObjects(objectsToTest)
+
+  // // reset all the objects color
+  // for (const object of objectsToTest) {
+  //   object.material.color.set('#ff0000')
+  // }
+
+  // // only update the color of objects that intersect the ray
+  // for (const intersect of intersects) {
+  //   intersect.object.material.color.set('#0000ff')
+  // }
+
+  if (intersects.length) {
+    if (!currentIntersect) {
+      console.log('mouse enter')
+    }
+
+    currentIntersect = intersects[0]
+  } else {
+    if (currentIntersect) {
+      console.log('mouse leave')
+    }
+
+    currentIntersect = null
+  }
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
+
+window.addEventListener('click', () => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log('click on object 1')
+        break;
+      case object2:
+        console.log('click on object 2')
+        break;
+      case object3:
+        console.log('click on object 3')
+        break;
+    }
+  }
+})
