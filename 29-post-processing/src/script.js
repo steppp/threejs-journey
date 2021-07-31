a28856ca6e2ace924e2d57cb3be7445a555d164f
Stefano Andriolo <stefano@srand.it>
2021-07-31 12:42:54 +0200

    Add exercise 29 (WIP)

---
+++ 29-post-processing/src/script.js
@@ -0,0 +1,255 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as dat from 'dat.gui'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass'
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader'
+import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Loaders
+ */
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const textureLoader = new THREE.TextureLoader()
+
+/**
+ * Update all materials
+ */
+const updateAllMaterials = () => {
+  scene.traverse(child => {
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      child.material.envMapIntensity = 5
+      child.material.needsUpdate = true
+      child.castShadow = true
+      child.receiveShadow = true
+    }
+  })
+}
+
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+  '/textures/environmentMaps/0/px.jpg',
+  '/textures/environmentMaps/0/nx.jpg',
+  '/textures/environmentMaps/0/py.jpg',
+  '/textures/environmentMaps/0/ny.jpg',
+  '/textures/environmentMaps/0/pz.jpg',
+  '/textures/environmentMaps/0/nz.jpg',
+])
+environmentMap.encoding = THREE.sRGBEncoding
+
+scene.background = environmentMap
+scene.environment = environmentMap
+
+/**
+ * Models
+ */
+gltfLoader.load('/models/DamagedHelmet/glTF/DamagedHelmet.gltf', gltf => {
+  gltf.scene.scale.set(2, 2, 2)
+  gltf.scene.rotation.y = Math.PI * 0.5
+  scene.add(gltf.scene)
+
+  updateAllMaterials()
+})
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 3, -2.25)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  // Update effect composer
+  effectComposer.setSize(sizes.width, sizes.height)
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.set(4, 1, -4)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 1.5
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// create a custom render target to fix the increased darkness
+// introduced by RGBShiftPass
+// note that rendering on this render target is not antialiased
+// by default so we have to implement that
+// const renderTarget = new THREE.WebGLMultisampleRenderTarget(
+
+let RenderTargetClass = null
+if (renderer.getPixelRatio() === 1 && renderer.capabilities.isWebGL2) {
+  // enable the multisample render target where it is supported
+  // if the pixel ratio is 1
+  RenderTargetClass = THREE.WebGLMultisampleRenderTarget
+  console.log('Using WebGLMultisampleRenderTarget')
+} else {
+  // pixel ratio above 1 => antialias not needed
+  RenderTargetClass = THREE.WebGLRenderTarget
+  console.log('Using WebGLRenderTarget')
+}
+const renderTarget = new RenderTargetClass(
+  800,
+  600,
+  {
+    minFilter: THREE.LinearFilter,
+    magFilter: THREE.LinearFilter,
+    format: THREE.RGBAFormat,
+    encoding: THREE.sRGBEncoding
+  }
+)
+
+if (renderer.getPixelRatio() === 1 && !renderer.capabilities.isWebGL2) {
+  // use the SMAAPass (expensive) where WebGL2 is not supported
+  // it the pixel ratio is 1
+  const smaaPass = new SMAAPass()
+  effectComposer.addPass(smaaPass)
+
+  console.log('Using SMAA')
+}
+
+// this renders frames using ping-pong buffering
+// while it renders one the first buffer it takes the texture
+// to work on of the second one, then switch and repeat
+const effectComposer = new EffectComposer(renderer, renderTarget)
+effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+effectComposer.setSize(sizes.width, sizes.height)
+
+/**
+ * Passes
+ */
+const renderPass = new RenderPass(scene, camera)
+effectComposer.addPass(renderPass)
+
+const dotScreenPass = new DotScreenPass()
+dotScreenPass.enabled = false
+effectComposer.addPass(dotScreenPass)
+
+const glitchPass = new GlitchPass()
+glitchPass.enabled = false
+glitchPass.goWild = false
+effectComposer.addPass(glitchPass)
+
+// RGBShift only available as a shader
+// so we need to import the shader and apply it to a ShaderPass
+// this the same that happens with other passes automatically
+// note that this shader causes the scene to become darker
+// the reason is that the output encoding of the renderer does not
+// work anymore
+const rgbShiftPass = new ShaderPass(RGBShiftShader)
+rgbShiftPass.enabled = false
+effectComposer.addPass(rgbShiftPass)
+
+const unrealBloomPass = new UnrealBloomPass()
+effectComposer.addPass(unrealBloomPass)
+
+unrealBloomPass.strength = 0.3
+unrealBloomPass.radius = 1
+unrealBloomPass.threshold = 0.6
+
+gui.add(unrealBloomPass, 'enabled')
+gui.add(unrealBloomPass, 'strength').min(0).max(2).step(0.001)
+gui.add(unrealBloomPass, 'radius').min(0).max(2).step(0.001)
+gui.add(unrealBloomPass, 'threshold').min(0).max(1).step(0.001)
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let elapsedTime;
+
+const debugObject = {
+  logVariables: () => {
+    console.log(elapsedTime)
+  }
+}
+gui.add(debugObject, 'logVariables');
+
+const tick = () => {
+  elapsedTime = clock.getElapsedTime()
+
+  // Update controls
+  controls.update()
+
+  // Render
+  // renderer.render(scene, camera)
+  effectComposer.render()
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+}
+
+tick()
