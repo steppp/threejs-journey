5510f2af3dfdf392370d41e34ef977eb5b69dd65
Stefano Andriolo <stefano@srand.it>
2021-08-02 20:28:40 +0200

    Finish exercise 31

--- 31-intro-and-loading-progress/src/script.js
+++ 31-intro-and-loading-progress/src/script.js
@@ -2,12 +2,36 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { gsap } from 'gsap'
 
 /**
  * Loaders
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+const loadingBar = document.querySelector('.loading-bar')
+
+// use a shared loading manager to keep track of the global loading state
+const loadingManager = new THREE.LoadingManager(
+  // onLoad
+  () => {
+    // wait for the bar animation to finish to completely fill the screen's width
+    window.setTimeout(() => {
+      gsap.to(overlayMaterial.uniforms.uAlpha, {
+        duration: 3.0,
+        value: 0
+      })
+  
+      loadingBar.classList.add('ended')
+      loadingBar.style.transform = ''
+    }, 500)
+  },
+  // onProgress
+  (itemUrl, loadedItems, totalItems) => {
+    const progressRatio = loadedItems / totalItems
+    loadingBar.style.transform = `scaleX(${progressRatio})`
+  }
+)
+const gltfLoader = new GLTFLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
 /**
  * Base
@@ -80,6 +104,36 @@ directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.normalBias = 0.05
 directionalLight.position.set(0.25, 3, -2.25)
 scene.add(directionalLight)
+
+/**
+ * Overlay
+ */
+// coordinates go from -1 to 1
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1)
+// use a shader material to place a fixed-position element in front of the camera
+// using a shader allows us to ignore the model view and projection matrices
+// and always render the shader at the same position
+const overlayMaterial = new THREE.ShaderMaterial({
+  transparent: true,
+  uniforms: {
+    uAlpha: { value: 1 }
+  },
+  vertexShader: `
+  void main()
+  {
+    gl_Position = vec4(position, 1.0);
+  }
+  `,
+  fragmentShader: `
+  uniform float uAlpha;
+  void main()
+  {
+    gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+  }
+  `
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+scene.add(overlay)
 
 /**
  * Sizes
